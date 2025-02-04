32af84f803880655ca7906f2c67e9a305765337b
Behnam SeydAbadi <behnamsa97@protonmail.com>
2025-02-04 21:31:54 +0330

    Pass "Create phone number successfully"

---
+++ Mc2.CrudTest.Application/Customer/Query/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Mc2.CrudTest.Application.Customer.Query.ViewModels;
+
+public class CustomerViewModel
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public required string BankAccountNumber { get; set; }
+}
---
+++ Mc2.CrudTest.Domain.Tests/Customer/CustomerTests.cs
@@ -0,0 +1,21 @@
+﻿using Mc2.CrudTest.Domain.Customer.ValueObjects;
+
+namespace Mc2.CrudTest.Domain.Tests.Customer;
+
+public class CustomerTests
+{
+    [Theory(DisplayName = "Create phone number successfully")]
+    [InlineData("+1-123-456-7890")]
+    [InlineData("(234) 567-8901")]
+    [InlineData("345.678.9012")]
+    [InlineData("456 789 0123")]
+    [InlineData("567-890-1234")]
+    [InlineData("+44 678 901 2345")]
+    [InlineData("(890) 123-4567")]
+    [InlineData("901-234-5678")]
+    [InlineData("012.345.6789")]
+    public void CreatePhoneNumberSuccessfully(string value)
+    {
+        PhoneNumberValueObject.Create(value);
+    }
+}
