f67209ddfb93373074c36b58c44107205eb43f2d
Behnam SeydAbadi <behnamsa97@protonmail.com>
2025-02-04 21:13:38 +0330

    Implement event handlers

---
+++ Mc2.CrudTest.Infrastructure/EventStore/Customer/CustomerCreatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Mc2.CrudTest.Domain.Customer.Events;
+using Mc2.CrudTest.Infrastructure.EventStore.Repository;
+using MediatR;
+
+namespace Mc2.CrudTest.Infrastructure.EventStore.Customer;
+
+public class CustomerCreatedEventHandler : INotificationHandler<CustomerCreatedEvent>
+{
+    private readonly IEventStoreRepository _eventStoreRepository;
+
+    public CustomerCreatedEventHandler(IEventStoreRepository eventStoreRepository)
+    {
+        _eventStoreRepository = eventStoreRepository;
+    }
+
+    public async Task Handle(CustomerCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        await _eventStoreRepository.AppendEventAsync(notification.AggregateId.ToString(), notification);
+        Console.WriteLine("Domain event stored: " + notification.GetType().Name);
+    }
+}
---
+++ Mc2.CrudTest.Infrastructure/ReadSide/Customer/EventHandlers/UpdateReadModelOnCustomerCreatedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Mc2.CrudTest.Domain.Customer;
+using Mc2.CrudTest.Domain.Customer.Events;
+using MediatR;
+
+namespace Mc2.CrudTest.Infrastructure.ReadSide.Customer.EventHandlers;
+
+public class UpdateReadModelOnCustomerCreatedEventHandler : INotificationHandler<CustomerCreatedEvent>
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public UpdateReadModelOnCustomerCreatedEventHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task Handle(CustomerCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        var customerDomainModel = CustomerDomainModel.Reconstruct(notification);
+
+        await _customerRepository.AddAsync(customerDomainModel);
+        Console.WriteLine("Domain event projected: " + notification.GetType().Name);
+    }
+}
