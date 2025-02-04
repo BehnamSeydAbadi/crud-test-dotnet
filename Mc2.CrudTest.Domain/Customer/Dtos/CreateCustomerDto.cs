ef9e134ca4f2b3a29ad2310649e52181d4a4c655
Behnam SeydAbadi <behnamsa97@protonmail.com>
2025-02-04 21:13:08 +0330

    Implement "CreateCustomerDto" and "CustomerCreatedEvent" in domain layer

---
+++ Mc2.CrudTest.Domain/Customer/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mc2.CrudTest.Domain.Customer.Dtos;
+
+public class CreateCustomerDto : ICustomerBaseModel
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public required string BankAccountNumber { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+}
---
+++ Mc2.CrudTest.Domain/Customer/Events/CustomerCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Mc2.CrudTest.Domain.Common;
+using MediatR;
+
+namespace Mc2.CrudTest.Domain.Customer.Events;
+
+public record CustomerCreatedEvent(
+    Guid AggregateId,
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    string Email,
+    string BankAccountNumber,
+    DateTime DateOfBirth
+) : AbstractDomainEvent(AggregateId), ICustomerBaseModel, INotification;
